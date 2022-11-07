2ac9571a438ad1b89c8bb7a5bdee75ec4042080a
Unidade <68622249+Unidade@users.noreply.github.com>
2022-11-07 11:27:33 -0300

    Fix types and clean the code

--- socket/webSocketServer.ts
+++ socket/webSocketServer.ts
@@ -1,48 +1,44 @@
-import { config } from "dotenv";
-import express from "express";
-import ws from "ws";
-import { parseData } from "../helpers/parseExternalDate";
-import { Server } from "socket.io";
-import { Express } from "express";
+import ws from 'ws'
+import { parseData } from '../helpers/parseExternalDate'
+import { Server } from 'socket.io'
+import type { Server as HttpServer } from 'http'
 
-
-export const startWebScocketServer = (server: any) => {
-  config();
-  const port = process.env.PORT;
+export const startWebScocketServer = (server: HttpServer) => {
+  const port = process.env.PORT
 
   //Start the backend server socket
   const backendSocket = new Server(server, {
     cors: {
-      origin: "*",
+      origin: '*',
     },
-  });
+  })
 
   server.listen(port, () => {
-    console.log("Websocket Server is up & running", port);
-  });
+    console.log('Websocket Server is up & running', port)
+  })
 
   // Start external API socket
   const connectExternalAPI = () => {
     const tradeSocket = new ws(
       `wss://ws.twelvedata.com/v1/quotes/price?apikey=${process.env.API_KEY}`
-    );
+    )
 
     // Subscribe message to start getting values
     const subscribe = {
-      action: "subscribe",
+      action: 'subscribe',
       params: {
-        symbols: "EUR/USD,USD/JPY,BTC/USD",
+        symbols: 'EUR/USD,USD/JPY,BTC/USD',
       },
-    };
+    }
     // Connection opened
-    tradeSocket.on("open", (event:any) => {
-      tradeSocket.send(JSON.stringify(subscribe));
-    });
+    tradeSocket.on('open', (event: any) => {
+      tradeSocket.send(JSON.stringify(subscribe))
+    })
 
     // Listen for messages and emit it using the backedSocket
-    tradeSocket.on("message", (data: any) => {
-      backendSocket.emit("message", parseData(data));
-    });
-  };
-  connectExternalAPI();
-};
+    tradeSocket.on('message', (data: any) => {
+      backendSocket.emit('message', parseData(data))
+    })
+  }
+  connectExternalAPI()
+}
